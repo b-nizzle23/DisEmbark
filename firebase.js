c7d211d6618a9c5a0fa0986691cfa37a55e625c9
Elsa Schutfort <a02392875@aggies.usu.edu>
2024-02-10 16:26:25 -0700

    Added config

---
+++ firebase.js
@@ -0,0 +1,13 @@
+import * as firebase from "firebase/app";
+import "firebase/analytics"
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBWrRcLscTATvz_btmABiczkULY_I6SEt4",
+  authDomain: "dis-embark-xvnpzx.firebaseapp.com",
+  projectId: "dis-embark-xvnpzx",
+  storageBucket: "dis-embark-xvnpzx.appspot.com",
+  messagingSenderId: "135887322005",
+  appId: "1:135887322005:web:cfe2f6c2446ceb33b9149c"
+};
+
+firebase.initializeApp(firebaseConfig);
