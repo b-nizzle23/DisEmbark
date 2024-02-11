aa58116be77ba5163275b01c532ccef308f565fc
Brighton E <brighton.ellis@gmail.com>
2024-02-11 02:25:57 -0700

    back arrow from account goes to mappage

--- app-skeleton/src/UserInfo.jsx
+++ app-skeleton/src/UserInfo.jsx
@@ -28,9 +28,9 @@ function UserInfoPage() {
   return (
     <>
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
+    {/* <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" /> */}
     <div>
-      <button className="material-symbols-outlined" onClick={handleExit}>
+      <button className="material-symbols-outlined" onClick={() => navigate('/mappage')}>
         close
       </button>
       <div className='user-pic-name'>
