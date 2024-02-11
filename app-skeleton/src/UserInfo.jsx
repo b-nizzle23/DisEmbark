14c02af5a2c8314ac5f9ad65d924f8c5c2de2fc0
Elsa Schutfort <a02392875@aggies.usu.edu>
2024-02-10 20:12:44 -0700

    Added CSS

--- app-skeleton/src/UserInfo.jsx
+++ app-skeleton/src/UserInfo.jsx
@@ -1,19 +1,33 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import {getAuth } from 'firebase/auth'; 
+import { useState } from 'react';
 
 function UserInfoPage() {
+  const [logout, setLogOut] = useState(false);
+  const auth = getAuth();
+  const navigate = useNavigate();
+
+  function handleLogOut(e) {
+    auth.signOut;
+    return navigate("/homepage");
+  }
   return (
+    <>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
     <div>
       <h2>User Info Page</h2>
+      <span className="material-symbols-outlined">
+        close
+      </span>
       <div className="menu">
-        <ul>
-          <Link to="/mypins" className="my-pins">My Pins</Link>
-          <li><a href="#request-feature">Request Feature</a></li>
-          <li><a href="#my-pins">My Pins</a></li>
-        </ul>
+        <div><Link to="/mypins" className="my-pins">My Pins</Link></div>
+        <Link to="/settings">Account Settings</Link>
+        <button onClick={handleLogOut}>Log Out</button>
       </div>
 
-      </div>
+    </div>
+    </>
   );
 }
 
